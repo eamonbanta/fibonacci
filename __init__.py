b63f9d929ff5f25d54b89a9f71f3419d1c230f9f
Eamon Banta <eamon.banta@venmo.com>
2016-03-21 16:23:48 -0400

    add main() method with one choice: quit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,29 @@
-if __name__ == "__main__":
+import sys
+
+
+VALID_CHOICES = {
+    'q': '(q)uit',
+}
+
+
+def main():
     print "How do you want to calculate the fibonacci numbers?"
+    print "Possible choices are:"
+    print "(q)uit"
 
     fib_type = raw_input().strip()
 
-    print "Thanks for trying, but nothing is implemented yet."
+    while fib_type not in VALID_CHOICES:
+        print "Sorry, that's an invalid choice. Try again."
+        print "Possible choices are:"
+        print "(q)uit"
+        fib_type = raw_input().strip()
+
+    if fib_type != 'q':
+        print "Great!"
+
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
