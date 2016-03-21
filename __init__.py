edb3bda9ff639d423e380f8f64415ae9ce7b2e8c
Eamon Banta <eamon.banta@venmo.com>
2016-03-21 15:30:09 -0400

    add stub for fibonacci UI

---
+++ __init__.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print "How do you want to calculate the fibonacci numbers?"
+
+    fib_type = raw_input().strip()
+
+    print "Thanks for trying, but nothing is implemented yet."
